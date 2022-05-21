dcf9111a633f668c98cb37f6dd813e405a87aa54
Yunus Emre Şahin <yunusemre.sahin@ogr.iu.edu.tr>
2022-05-21 23:34:40 +0300

    Add project files.

---
+++ DictionaryIntro/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryIntro
+{
+    class MyDictionary<T,X>
+    {
+
+        T[] items;
+        X[] items2;
+        public MyDictionary()
+        {
+            items = new T[0];
+            items2 = new X[0];
+
+        }
+
+        public void Add(T item , X item2)
+        {
+            T[] tempList = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempList.Length; i++)
+            {
+                items[i] = tempList[i];
+            }
+            items[items.Length - 1] = item;
+
+            X[] tempList2 = items2;
+            items2 = new X[items2.Length + 1];
+            for (int i = 0; i < tempList2.Length; i++)
+            {
+                items2[i] = tempList2[i];
+            }
+            items2[items2.Length - 1] = item2;
+        }
+        public int Lenght { get { return items.Length; } }
+
+        public T[] Key { get { return items; } }
+        public X[] Name { get { return items2; } }
+    }
+}
---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> personlist = new Dictionary<int, string>();
+            personlist.Add(1, "Mahmut");
+            personlist.Add(2, "Mehmet");
+            personlist.Add(4, "Ayşe");
+            personlist.Add(6, "Fatma");
+
+            foreach (var item in personlist)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            MyDictionary<int, string> personlist2 = new MyDictionary<int, string>();
+            personlist2.Add(1, "Mahmut");
+            personlist2.Add(2, "Mehmet");
+            personlist2.Add(4, "Ayşe");
+            personlist2.Add(6, "Fatma");
+            Console.WriteLine("Orjinal bitti.");
+            for (int i = 0; i < personlist2.Lenght; i++)
+            {
+                Console.WriteLine("[" + personlist2.Key[i] + ", " + personlist2.Name[i] + "]");
+                
+            }
+
+        }
+    }
+}
